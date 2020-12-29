39db50a8deb9a24d74510cd53ba5ebbf0ac46d48
Ankit <you@example.com>
2020-12-30 01:54:33 +0530

    New changes | removed ajax

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,34 +1,46 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+    $("#hel").on("click", function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log($("#comment").val());
+        // $.ajax({
+        //     url: "/blogs/" + $("#commentForm").data("blog") + "/comments/new",
+        //     type: "POST",
+        //     data: { text: "Ankit" },
+        //     success: function (result) {
+        //         console.log(result);
+        //         console.log("Success!");
+        //         changeComments(result);
+        //         removeTextFromInput("comment");
+        //     },
+        //     error: function (jqXHR, textStatus, err) {
+        //         console.log("text status " + textStatus + ", err " + err);
+        //     },
+        // });
+        $.ajax({
+            url: "/blogs/" + $("#commentForm").data("blog") + "/comments/new",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({ comment: "Ankit" }),
+            // data: { text: $("#comment").val() },
+        });
+        // .done(function (result) {
+        //     changeComments(result);
+        //     removeTextFromInput("comment");
+        // })
+        // .fail(function (err) {
+        //     console.log("Ajax Error");
+        //     console.log(err);
+        // });
+    });
 
-$("#commentSubmit").on('click', function(event){
-	event.preventDefault();
-	event.stopPropagation();
-	console.log($("#comment").val());
-	$.ajax({
-		url:"/blogs/" + $("#commentForm").data('blog') + "/comments/new",
-		type:"POST",
-		contentType:"application/json",
-		//data: JSON.stringify({name:"Ankit"})
-		data:JSON.stringify({text:$("#comment").val()}),
-	}).done(function(result){
-		changeComments(result);
-		removeTextFromInput("comment");
-	}).fail(function(err){
-		console.log("Ajax Error");
-		console.log(err);
-	});
+    var changeComments = function (comments) {
+        var div = document.getElementById("commentsDiv");
+        div.innerHTML = comments;
+    };
+
+    var removeTextFromInput = function (id) {
+        var removeThis = document.getElementById(id);
+        removeThis.innerHTML = "";
+    };
 });
-
-var changeComments = function(comments){
-	var div = document.getElementById("commentsDiv");
-	div.innerHTML = comments;
-}
-
-var removeTextFromInput = function(id){
-	var removeThis = document.getElementById(id);
-	removeThis.innerHTML = "";
-	}
-
-});
-
-
--- routes/comments.js
+++ routes/comments.js
@@ -1,74 +1,81 @@
-var express = require('express');
-var router = express.Router({mergeParams: true});
+var express = require("express");
+var router = express.Router({ mergeParams: true });
 var Blog = require("../models/blog.js");
 var Comment = require("../models/comments.js");
 var middleware = require("../middleware/middleware.js");
 
-
 //NEW COMMENT
-router.post("/new",  function(req, res){ //middleware.isLoggedIn
-	console.log(req.body);
-	/*Blog.findById(req.params.id, function(err, blog){
-		if(err){
-			//console.log(err);
-			console.log("First Error");
-			res.redirect("/blogs");
-		}else{
-			Comment.create({text:req.body.comment}, function(err, comment){
-				if(err){
-					console.log("Second ERROR: ");
-					res.redirect("/blogs");
-				}else{
-					comment.author.id = req.user._id;
-					comment.author.username = req.user.username;
-					comment.save();
-					blog.comments.push(comment);
-					blog.save();
-					req.flash("success", "Successfully added comment");
-					res.render("/ajax_views/comments", {blog: blog});
-					res.end();
-				}
-			});
-			//console.log(blog);
-		}
-	}); */
+router.post("/new", middleware.isLoggedIn, function (req, res) {
+    //middleware.isLoggedIn
+    console.log("In comment");
+    console.log(req.body);
+    Blog.findById(req.params.id, function (err, blog) {
+        if (err) {
+            //console.log(err);
+            console.log("First Error");
+            res.redirect("/blogs");
+        } else {
+            Comment.create({ text: req.body.comment }, function (err, comment) {
+                if (err) {
+                    console.log("Second ERROR: ");
+                    res.redirect("/blogs");
+                } else {
+                    comment.author.id = req.user._id;
+                    comment.author.username = req.user.username;
+                    comment.save();
+                    blog.comments.push(comment);
+                    blog.save();
+                    req.flash("success", "Successfully added comment");
+                    // res.render("/ajax_views/comments", { blog: blog });
+                    res.redirect("/blogs/" + req.params.id);
+                }
+            });
+            //console.log(blog);
+        }
+    });
 });
 
 //EDIT COMMENT
-router.get("/:comment_id/edit", middleware.isLoggedIn, function(req, res){
-	Comment.findById(req.params.comment_id, function(err, foundComment){
-		if(err) console.log(err);
-		else{
-			res.render("comments/edit", { blog_id: req.params.id, comment: foundComment});
-		}
-	});
+router.get("/:comment_id/edit", middleware.isLoggedIn, function (req, res) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+        if (err) console.log(err);
+        else {
+            res.render("comments/edit", {
+                blog_id: req.params.id,
+                comment: foundComment,
+            });
+        }
+    });
 });
 
 //UPDATE
 
-router.put("/:comment_id", function(req, res){
-	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
-		if(err){
-			res.redirect("back");
-		}else{
-			req.flash("success", "Comment updated");
-			res.redirect("/blogs/" + req.params.id);
-		}
-		
-	});
+router.put("/:comment_id", function (req, res) {
+    Comment.findByIdAndUpdate(
+        req.params.comment_id,
+        req.body.comment,
+        function (err, updatedComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                req.flash("success", "Comment updated");
+                res.redirect("/blogs/" + req.params.id);
+            }
+        }
+    );
 });
 
 //DELETE COMMENT
 
-router.delete("/:comment_id",   function(req, res){
-	Comment.findByIdAndRemove(req.params.comment_id, function(err){
-		if(err){
-			res.redirect("back");
-		}else{
-			req.flash("success", "Successfully deleted comment");
-			res.redirect("/blogs/" + req.params.id);
-		}
-	});
+router.delete("/:comment_id", function (req, res) {
+    Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            req.flash("success", "Successfully deleted comment");
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
 });
 
 module.exports = router;
